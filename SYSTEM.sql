b7f9132fcd8496b9bbf55ba50407ae412d07ccaf
sergio <sdlbsso@gmail.com>
2024-05-30 12:38:56 -0300

    system creacion y permisos de usuario

---
+++ SYSTEM.sql
@@ -0,0 +1,6 @@
+--create USER sergio IDENTIFIED BY admin DEFAULT TABLESPACE system TEMPORARY TABLESPACE temp QUOTA UNLIMITED ON system;
+grant create session to sergio;
+grant create table to sergio;
+grant create view to sergio;
+grant create PROCEDURE to sergio;
+grant create sequence to sergio;
