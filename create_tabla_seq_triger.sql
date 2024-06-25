38a2339ec6e60c25a7f662da3be29ed7011cfe56
sergio <sdlbsso@gmail.com>
2024-06-25 11:01:24 -0300

    creacion de tabla, secuencia y triger para id

---
+++ create_tabla_seq_triger.sql
@@ -0,0 +1,26 @@
+create table productos(
+id int not null primary key,
+descripcion varchar2(30) not null,
+precio number(6,2) default 0,
+existencia int default 0
+);
+
+create sequence seq_id_productos
+start with 1
+increment by 1
+maxvalue 9999
+minvalue 1
+cycle;
+
+
+CREATE OR REPLACE TRIGGER trg_incremento_id_productos
+BEFORE INSERT ON productos
+FOR EACH ROW
+BEGIN
+  -- Asigna el próximo valor de la secuencia al id
+  :NEW.id := seq_id_productos.NEXTVAL;
+END;
+
+insert into productos(descripcion,precio,existencia)values('autos',3500,'100');
+
+select * from productos;
